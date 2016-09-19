90072a904e88f751ef8a74d3b1e97841439ccd62
ccchangkong <chlo0o@qq.com>
2016-09-19 18:06:04 +0800

    手机端注册登录重构

---
+++ Login/js/login.js
@@ -0,0 +1,9 @@
+﻿$(function() {
+	$(".lMainUlLiInput").on('click', function() {
+		$(this).closest('.lMainUlLi').addClass('lMainUlLiAct').siblings('.lMainUlLi').removeClass('lMainUlLiAct');
+		$(this).siblings('i').addClass($(this).siblings('i').data('class'));
+
+//		alert($(this).siblings('i').addClass($(this).siblings('i').data('class')).closest('.lMainUlLi').siblings('.lMainUlLi').find('i').removeClass($(this).data('class'));
+
+	})
+});
---
+++ Login/js/resetPwd.js
@@ -0,0 +1,9 @@
+﻿$(function() {
+	$(".rMainUlLiInput").on('click', function() {
+		$(this).closest('.rMainUlLi').addClass('lMainUlLiAct').siblings('.rMainUlLi').removeClass('rMainUlLiAct');
+//		$(this).siblings('i').addClass($(this).siblings('i').data('class'));
+
+//		alert($(this).siblings('i').addClass($(this).siblings('i').data('class')).closest('.rMainUlLi').siblings('.rMainUlLi').find('i').removeClass($(this).data('class'));
+
+	})
+});
